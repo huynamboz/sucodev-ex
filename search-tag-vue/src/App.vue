5e059014e12a046fd360df0d6f7ea6a9308ac5b6
Mac <mac@192.168.1.76>
2024-10-14 19:04:25 +0700

    feat: search tag code vue

---
+++ search-tag-vue/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from 'vue';
+
+const languages = ref(['html', 'css', 'javascript'])
+
+const data = ref('')
+
+function handleEnter() {
+  languages.value.push(data.value)
+  data.value = ''
+}
+
+function handleDelete(index) {
+  languages.value.splice(index, 1)
+}
+</script>
+<template>
+  <div class="container">
+
+    <div class="list">
+
+      <!-- item -->
+      <div class="item" v-for="(language, index) in languages">
+        <p>{{ language }}</p>
+        <svg @click="handleDelete(index)" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"/></svg>
+      </div>
+      <input type="text" v-model="data" @keyup.enter="handleEnter">
+
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  padding: 20px;
+}
+
+.list {
+  border: 1px solid #7a7a7a;
+  padding: 10px;
+  border-radius: 6px;
+  margin-top: 10px;
+  display: flex;
+  gap: 6px;
+  flex-wrap: wrap;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  background-color: #b5b5b5;
+  width: fit-content;
+  padding: 4px;
+  border-radius: 4px;
+}
+</style>
---
+++ search-tag-vue/src/Document.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from 'vue';
+
+const languages = ref(
+  [
+    "html",
+    "css",
+    "javascript",
+    "php"
+  ]
+)
+
+// languages.forEach((item, index) => {
+  
+// });
+
+const notifications = ref([
+  {
+    message: "Notice 1",
+    hide: true,
+  },
+  {
+    message: "Notice 2",
+    hide: false
+  },
+  {
+    message: "Notice 3",
+    hide: false
+  }
+])
+
+const person = ref({
+  name: "hinam",
+  birthday: "1/1/1111"
+})
+
+const isEnable = ref(true)
+</script>
+<template>
+  <div>
+    <ul>
+      <li v-for="(item) in languages">
+        {{ item }} - 
+        <!-- {{ index * 100 }} -->
+      </li>
+    </ul>
+
+    <p>notifications</p>
+
+    <!-- destructuring -->
+    <button v-for="noti in notifications">
+      <p v-if="noti.hide === false">{{ noti.message }}</p>
+    </button>
+    <br>
+    
+    <template v-for="noti in notifications">
+      <button v-if="noti.hide === false">
+        <p>{{ noti.message }}</p>
+      </button>
+    </template>
+
+    <!-- loop through object -->
+     <div>
+      <p>Name: {{ person.name }}</p>
+      <ul>
+        <li v-for="(val, key, index) in person">
+          {{ key }} : {{ val }} - {{ index }}
+        </li>
+      </ul>
+     </div>
+
+     <button v-for="n in 10">
+      button - {{ n }}
+     </button>
+
+     <!-- fragment -->
+      <template v-for="n in 10">
+        <button>asd</button>
+        <div>{{ n }}</div>
+        <input type="text">
+      </template>
+
+
+      <button v-if="isEnable === true">SUBMIT</button>
+      <p v-else-if="isEnable = false">button submit disabled</p>
+      <p v-else></p>
+      <button @click="isEnable = !isEnable">Disable submit {{ isEnable }}</button>
+  </div>
+</template>
